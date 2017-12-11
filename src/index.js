7bca4020abbe5865f4853f72e4a0ddcab72fdc76
Sébastien Elet <sebastien@elet.fr>
2017-12-11 15:09:28 +0100

    style(*): fix linting errors

--- src/index.js
+++ src/index.js
@@ -15,63 +15,77 @@ function checkExpression(errors, conditions, expression) {
   conditions.some(function checkConditions(condition, index) {
     var conditionErrors = checkCondition(condition, index, expression);
 
-    debug('Condition check of:', condition, ' led to ', conditionErrors.length, ' errors.');
-    if(conditionErrors.length) {
+    debug(
+      'Condition check of:',
+      condition,
+      ' led to ',
+      conditionErrors.length,
+      ' errors.'
+    );
+    if (conditionErrors.length) {
       expressionErrors = expressionErrors.concat(conditionErrors);
-    } else {
-      expressionErrors = conditionErrors;
-      return true;
+      return false;
     }
+    expressionErrors = conditionErrors;
+    return true;
   });
-  if(expressionErrors.length) {
-    errors.push(new YError(
-      'E_BAD_EXPRESSION',
-      expression,
-      expressionErrors.length,
-      expressionErrors
-    ));
+  if (expressionErrors.length) {
+    errors.push(
+      new YError(
+        'E_BAD_EXPRESSION',
+        expression,
+        expressionErrors.length,
+        expressionErrors
+      )
+    );
   }
   // By-pass child expressions checking when having a custom parser
   // Warning: Using this feature may cause security issues!
-  if(conditions.some(function(condition) {
-    return condition.type === expression.type && condition.$_;
-  })) {
+  if (
+    conditions.some(function(condition) {
+      return condition.type === expression.type && condition.$_;
+    })
+  ) {
     debug('Child expression cancelled with a custom parser:', expression);
     return errors;
   }
   // Check child expressions
-  if('Program' === expression.type) {
-    errors = expression.body.reduce(function processProgram(errors, expression) {
+  if ('Program' === expression.type) {
+    errors = expression.body.reduce(function processProgram(
+      errors,
+      expression
+    ) {
       return errors.concat(checkExpression(errors, conditions, expression));
-    }, errors);
-  } else if('ExpressionStatement' === expression.type) {
+    },
+    errors);
+  } else if ('ExpressionStatement' === expression.type) {
     errors = checkExpression(errors, conditions, expression.expression);
-  } else if('BinaryExpression' === expression.type) {
+  } else if ('BinaryExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.left);
     errors = checkExpression(errors, conditions, expression.right);
-  } else if('LogicalExpression' === expression.type) {
+  } else if ('LogicalExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.left);
     errors = checkExpression(errors, conditions, expression.right);
-  } else if('UnaryExpression' === expression.type) {
+  } else if ('UnaryExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.argument);
-  } else if('ConditionalExpression' === expression.type) {
+  } else if ('ConditionalExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.test);
     errors = checkExpression(errors, conditions, expression.consequent);
     errors = checkExpression(errors, conditions, expression.alternate);
-  } else if('NewExpression' === expression.type) {
+  } else if ('NewExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.callee);
     errors = expression.arguments.reduce(function(errors, expression) {
       return errors.concat(checkExpression(errors, conditions, expression));
     }, errors);
-  } else if('MemberExpression' === expression.type) {
+  } else if ('MemberExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.object);
     errors = checkExpression(errors, conditions, expression.property);
-  } else if('CallExpression' === expression.type) {
+  } else if ('CallExpression' === expression.type) {
     errors = checkExpression(errors, conditions, expression.callee);
     errors = expression.arguments.reduce(function(errors, expression) {
       return errors.concat(checkExpression(errors, conditions, expression));
     }, errors);
-  } else if(-1 === ['Identifier', 'Literal'].indexOf(expression.type)) {
+  } else if (-1 === ['Identifier', 'Literal'].indexOf(expression.type)) {
     errors = [new YError('E_UNHANDLED_EXPRESSION', expression.type)];
   }
   return errors;
@@ -80,7 +94,7 @@ function checkExpression(errors, conditions, expression) {
 function checkCondition(condition, index, expression) {
   var errors = [];
 
-  if(!condition.type) {
+  if (!condition.type) {
     throw new Error('E_NO_CONDITION_TYPE');
   }
 
@@ -88,7 +102,7 @@ function checkCondition(condition, index, expression) {
   errors = checkConditionKey(condition, expression, errors, 'type');
 
   // If type doesn't match, do not parse the all condition keys
-  if(errors.length) {
+  if (errors.length) {
     return errors;
   }
 
@@ -103,33 +117,53 @@ function checkCondition(condition, index, expression) {
 }
 
 function checkConditionKey(condition, expression, errors, property) {
-
   // Simple value based condition
-  if(-1 !== ['boolean', 'string', 'number'].indexOf(typeof condition[property])) {
-    if(condition[property] !== expression[property]) {
-      errors.push(new YError('E_DIFFERENT_VALUES', condition[property], expression[property]));
+  if (
+    -1 !== ['boolean', 'string', 'number'].indexOf(typeof condition[property])
+  ) {
+    if (condition[property] !== expression[property]) {
+      errors.push(
+        new YError(
+          'E_DIFFERENT_VALUES',
+          condition[property],
+          expression[property]
+        )
+      );
     }
-  // RegExp based condition
-  } else if(condition[property] instanceof RegExp) {
-    if(!condition[property].test(expression[property])) {
-      errors.push(new YError('E_NO_MATCH', condition[property], expression[property]));
+    // RegExp based condition
+  } else if (condition[property] instanceof RegExp) {
+    if (!condition[property].test(expression[property])) {
+      errors.push(
+        new YError('E_NO_MATCH', condition[property], expression[property])
+      );
     }
-  // Array based condition
-  } else if(condition[property] instanceof Array) {
-    if(-1 === condition[property].indexOf(expression[property])) {
-      errors.push(new YError('E_NO_MATCH', condition[property], expression[property]));
+    // Array based condition
+  } else if (condition[property] instanceof Array) {
+    if (-1 === condition[property].indexOf(expression[property])) {
+      errors.push(
+        new YError('E_NO_MATCH', condition[property], expression[property])
+      );
     }
-  // Expression check based condition
-  } else if('$_' === property) {
-    if(!(condition[property] instanceof Function)) {
-      throw new YError('E_BAD_EXPRESSION_CHECKER', property, condition[property]);
+    // Expression check based condition
+  } else if ('$_' === property) {
+    if (!(condition[property] instanceof Function)) {
+      throw new YError(
+        'E_BAD_EXPRESSION_CHECKER',
+        property,
+        condition[property]
+      );
     }
     errors = errors.concat(condition[property](expression));
-  // Bad condition property
+    // Bad condition property
   } else {
-    throw new YError('E_BAD_CONDITION_PROPERTY', condition, property, condition[property]);
+    throw new YError(
+      'E_BAD_CONDITION_PROPERTY',
+      condition,
+      property,
+      condition[property]
+    );
   }
-  if(errors.length) {
+  if (errors.length) {
     debug('Property check:', property, 'led to', errors);
   }
   return errors;
--- tests/index.mocha.js
+++ tests/index.mocha.js
@@ -9,127 +9,137 @@ function getErrorCode(error) {
 }
 
 describe('jsub', function() {
-
   describe('with autorized contents', function() {
-
     it('should work with simple arithmetics', function() {
       var script = '1 + 1';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '+',
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '+',
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast),
-        []
-      );
+      assert.deepEqual(jsub(syntax, ast), []);
     });
 
     it('should work with defined functions calls', function() {
       var script = '1 + 1 + taskValue("abbacacaabbacacaabbacaca")';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '+',
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }, {
-          type: 'CallExpression',
-          $_: function(expression) {
-            return expression.callee &&
-              'Identifier' === expression.callee.type &&
-              'taskValue' === expression.callee.name &&
-              1 === expression.arguments.length &&
-              'Literal' === expression.arguments[0].type &&
-              (/^([0-9a-f]{24})$/).test(expression.arguments[0].value) ?
-              [] :
-              [new Error('E_UNEXPECTED')];
+        conditions: [
+          {
+            type: 'Program',
           },
-        }],
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '+',
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+          {
+            type: 'CallExpression',
+            $_: function(expression) {
+              return expression.callee &&
+                'Identifier' === expression.callee.type &&
+                'taskValue' === expression.callee.name &&
+                1 === expression.arguments.length &&
+                'Literal' === expression.arguments[0].type &&
+                /^([0-9a-f]{24})$/.test(expression.arguments[0].value)
+                ? []
+                : [new Error('E_UNEXPECTED')];
+            },
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast),
-        []
-      );
+      assert.deepEqual(jsub(syntax, ast), []);
     });
 
     it('should work with array conds', function() {
       var script = '3 + 9 * 5 - 6';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: ['+', '-', '*', '/'],
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: ['+', '-', '*', '/'],
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast),
-        []
-      );
+      assert.deepEqual(jsub(syntax, ast), []);
     });
 
     it('should work with complexer nested expressions', function() {
-      var script = '(taskValue("abbacacaabbacacaabbacaca") > 1  ||' +
+      var script =
+        '(taskValue("abbacacaabbacacaabbacaca") > 1  ||' +
         ' taskValue("abbacacaabbacacaabbacaca") < 2) && (1+2 == 3)';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: ['>', '<', '+', '=='],
-        }, {
-          type: 'LogicalExpression',
-          operator: ['||', '&&'],
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }, {
-          type: 'CallExpression',
-          $_: function(expression) {
-            return expression.callee &&
-              'Identifier' === expression.callee.type &&
-              'taskValue' === expression.callee.name &&
-              1 === expression.arguments.length &&
-              'Literal' === expression.arguments[0].type &&
-              (/^([0-9a-f]{24})$/).test(expression.arguments[0].value) ?
-              [] :
-              [new Error('E_UNEXPECTED')];
+        conditions: [
+          {
+            type: 'Program',
           },
-        }],
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: ['>', '<', '+', '=='],
+          },
+          {
+            type: 'LogicalExpression',
+            operator: ['||', '&&'],
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+          {
+            type: 'CallExpression',
+            $_: function(expression) {
+              return expression.callee &&
+                'Identifier' === expression.callee.type &&
+                'taskValue' === expression.callee.name &&
+                1 === expression.arguments.length &&
+                'Literal' === expression.arguments[0].type &&
+                /^([0-9a-f]{24})$/.test(expression.arguments[0].value)
+                ? []
+                : [new Error('E_UNEXPECTED')];
+            },
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast),
-        []
-      );
+      assert.deepEqual(jsub(syntax, ast), []);
     });
 
     it('should work with the README sample', function() {
@@ -142,167 +152,192 @@ describe('jsub', function() {
             vegetables: [],
           },
         },
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: ['*', '-'],
-        }, {
-          type: 'Literal',
-          raw: /^[0-9]{1,5}$/,
-        }, {
-          type: 'CallExpression',
-          $_: function(expression) {
-            return expression.callee &&
-              'Identifier' === expression.callee.type &&
-              'lengthOf' === expression.callee.name &&
-              1 === expression.arguments.length &&
-              'Literal' === expression.arguments[0].type &&
-              (/^fruits|vegetables$/).test(expression.arguments[0].value) ?
-              [] :
-              [new Error('E_UNEXPECTED')];
+        conditions: [
+          {
+            type: 'Program',
           },
-        }],
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: ['*', '-'],
+          },
+          {
+            type: 'Literal',
+            raw: /^[0-9]{1,5}$/,
+          },
+          {
+            type: 'CallExpression',
+            $_: function(expression) {
+              return expression.callee &&
+                'Identifier' === expression.callee.type &&
+                'lengthOf' === expression.callee.name &&
+                1 === expression.arguments.length &&
+                'Literal' === expression.arguments[0].type &&
+                /^fruits|vegetables$/.test(expression.arguments[0].value)
+                ? []
+                : [new Error('E_UNEXPECTED')];
+            },
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast),
-        []
-      );
+      assert.deepEqual(jsub(syntax, ast), []);
     });
-
   });
 
   describe('with unautorized contents', function() {
-
-    it('should fail when some simple value conds aren\'t matching', function() {
+    it("should fail when some simple value conds aren't matching", function() {
       var script = '1 + 1';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '-', // Here is the failing syntax condition
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '-', // Here is the failing syntax condition
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast).map(getErrorCode),
-        ['E_BAD_EXPRESSION', 'E_BAD_EXPRESSION']
-      );
+      assert.deepEqual(jsub(syntax, ast).map(getErrorCode), [
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+      ]);
     });
 
     it('should fallback to security', function() {
       var script = 'if(1) {}';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'Identifier',
-        }, {
-          type: 'BlockStatement',
-        }, {
-          type: 'IfStatement',
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'Identifier',
+          },
+          {
+            type: 'BlockStatement',
+          },
+          {
+            type: 'IfStatement',
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast).map(getErrorCode),
-        ['E_UNHANDLED_EXPRESSION']
-      );
+      assert.deepEqual(jsub(syntax, ast).map(getErrorCode), [
+        'E_UNHANDLED_EXPRESSION',
+      ]);
     });
 
     it('should fail when trying to access window', function() {
       var script = 'document.cookies;';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '-', // Here is the failing syntax condition
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '-', // Here is the failing syntax condition
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast).map(getErrorCode),
-        [
-          'E_BAD_EXPRESSION', 'E_BAD_EXPRESSION',
-          'E_BAD_EXPRESSION', 'E_BAD_EXPRESSION',
-          'E_BAD_EXPRESSION', 'E_BAD_EXPRESSION',
-        ]
-      );
+      assert.deepEqual(jsub(syntax, ast).map(getErrorCode), [
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+      ]);
     });
 
-    it('should fail when some regExp conds aren\'t matching', function() {
+    it("should fail when some regExp conds aren't matching", function() {
       var script = '1 + 1';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '+',
-        }, {
-          type: 'Literal',
-          raw: /^(false|true)$/, // Here is the failing syntax condition
-        }],
+        conditions: [
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '+',
+          },
+          {
+            type: 'Literal',
+            raw: /^(false|true)$/, // Here is the failing syntax condition
+          },
+        ],
       };
 
-      assert.deepEqual(
-        jsub(syntax, ast).map(getErrorCode),
-        [
-          'E_BAD_EXPRESSION', 'E_BAD_EXPRESSION',
-          'E_BAD_EXPRESSION', 'E_BAD_EXPRESSION',
-        ]
-      );
+      assert.deepEqual(jsub(syntax, ast).map(getErrorCode), [
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+        'E_BAD_EXPRESSION',
+      ]);
     });
 
-    it('should throw when conds aren\'t recognized', function() {
+    it("should throw when conds aren't recognized", function() {
       var script = '1 + 1';
       var ast = esprima.parse(script, { loc: true });
       var syntax = {
-        conditions: [{
-          ahah: null,
-        }, {
-          type: 'Program',
-        }, {
-          type: 'ExpressionStatement',
-        }, {
-          type: 'BinaryExpression',
-          operator: '-', // Here is the failing syntax condition
-        }, {
-          type: 'Literal',
-          raw: /^([0-9]{1,5}|false|true)$/,
-        }],
+        conditions: [
+          {
+            ahah: null,
+          },
+          {
+            type: 'Program',
+          },
+          {
+            type: 'ExpressionStatement',
+          },
+          {
+            type: 'BinaryExpression',
+            operator: '-', // Here is the failing syntax condition
+          },
+          {
+            type: 'Literal',
+            raw: /^([0-9]{1,5}|false|true)$/,
+          },
+        ],
       };
 
       assert.throws(function() {
         jsub(syntax, ast);
       });
     });
-
   });
-
 });
